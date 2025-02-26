4c602a1c2f86621e8cdf03ea8cea7185348abf74
mhyjw98 <rhapsodymis4@naver.com>
2025-02-27 00:27:44 +0900

    Delete: 중복되는 클래스 삭제 후 새로 구현

--- Core/Commands/TextCommands.cs
+++ Core/Commands/TextCommands.cs
@@ -1,14 +1,14 @@
-﻿using Discord.Commands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Discord.Commands;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace MoongBot.Core.Commands
-{
-    public class TextCommands : ModuleBase<SocketCommandContext>
-    {
-        public static List<string> CommandToHide = new List<string>();
-    }
-}
+//namespace MoongBot.Core.Commands
+//{
+//    public class TextCommands : ModuleBase<SocketCommandContext>
+//    {
+//        public static List<string> CommandToHide = new List<string>();
+//    }
+//}
